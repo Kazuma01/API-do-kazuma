92cf65598489cabe9fa891e76eaa9d326acd0b60
kazuma01 <rm1584668@gmail.com>
2026-04-21 18:00:00 -0300

    API V2.01 front?

--- ApiV1.0/frontend/src/App.vue
+++ ApiV1.0/frontend/src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue';
 </script>
 
 <template>
-  <HelloWorld />
+  <Home />
 </template>
---
+++ ApiV1.0/frontend/src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>tabela de dados</h1>
+        <table border = "1">
+            <thead>
+                <tr>
+                    <th>Palavas</th>
+                    <th>Numeros</th>
+                </tr>
+            </thead>    
+            <tbody>
+                <tr v-for="(item,index) in ListaDDB" :key="index">
+                    <td>{{ item.Palavas }}</td>
+                    <td>{{ item.Numeros }}</td>
+                </tr>
+            </tbody>
+        </table>
+        
+        <button @click="ADD_dados">ADDress</button>
+    </div>
+    
+</template>
+<script>
+export default{
+    data(){
+        return{
+            ListaDDB: [
+                { Palavas: "kazuma", Numeros: 265543},
+                { Palavas: "kazuma", Numeros: 123678},
+                { Palavas: "kazuma", Numeros: 126583},
+                { Palavas: "kazuma", Numeros: 125123}
+            ]
+        }
+    },
+    methods: {
+        ADD_dados(){
+            this.ListaDDB.push({Palavas: "Kazua", Numeros: 123244})
+        }
+    }
+}
+</script>
