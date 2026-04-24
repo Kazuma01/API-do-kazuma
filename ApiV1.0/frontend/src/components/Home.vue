e09528a5f5d7bf2472769e744f5755e5bc177ebe
kazuma01 <kazumagt048@gmail.com>
2026-04-24 16:34:16 -0300

    API V2.498 front novamente

--- ApiV1.0/frontend/src/components/Home.vue
+++ ApiV1.0/frontend/src/components/Home.vue
@@ -1,38 +1,24 @@
+<template>
+    <div>
+        <H1>{{ mensagem }}</H1>
+        <button @click="Update">Button</button><br>
+        <input type="text" v-model="Texto" placeholder="Digita aqui"/><br>
+        <p>{{ Texto }}</p>
+    </div>
+</template>
+
 <script>
 export default{
     data(){
         return{
-            text: "KazuArch",
-            Text_com_red: "text_red",
-            contar: 0,
-            texto: ""
+            mensagem:"text",
+            Texto: ""
         }
     },
     methods:{
         Update(){
-            this.contar++,
-            this.text = "Arch Linux",
-            this.Text_com_red = "text_blue"
-        },
+            this.mensagem = "New text"
+        }
     }
 }
-
 </script>
-
-<template>
-    <div>
-        <h1 :class="Text_com_red">{{ text }}</h1>
-        <button @click="Update">mudou? {{ contar }}</button><br>
-        <input v-model="texto" placeholder="Digite aqui">
-        <p>{{ texto}}</p>
-    </div>
-</template>
-
-<style>
-.text_red{
-    color: blue;
-}
-.text_blue{
-    color: red;
-}
-</style>
