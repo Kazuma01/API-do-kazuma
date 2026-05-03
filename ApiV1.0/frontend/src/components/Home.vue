2e1ecdf836747062b516ed76cac1d55b23e6368d
kazuma <kazumagt048@gmail.com>
2026-05-03 06:38:54 -0300

    tudo normal_

--- ApiV1.0/frontend/src/components/Home.vue
+++ ApiV1.0/frontend/src/components/Home.vue
@@ -1,33 +1,7 @@
-<template>
-    <form @submit.prevent="NewItList">
-        <input type="text" v-model="Newitem" placeholder="ADD um novo texto"> <br>
-        <button>Add novo item</button>
-    </form>
-    <ul>
-        <li v-for="item in itemsList" :key="item.id">
-            {{ "(" + item.id + ") " + item.text }}
-            <button @click="RemverItemList(item)">X</button>
-        </li>
-    </ul>
-</template>
+<!-- https://pt.vuejs.org/tutorial/#step-7 -->
 
-<script>
-let id = 1
-export default{
-    data(){
-        return{ 
-            Newitem: " ",
-            itemsList: []
-        }
-    },
-    methods:{
-        NewItList(){
-            this.itemsList.push({id: id++,text: this.Newitem})
-            this.Newitem = ""
-        },
-        RemverItemList(item){
-            this.itemsList = this.itemsList.filter((t) => t != item)
-        }
-    }
-}
-</script>
+<template>
+    <div>
+        <h1>oiiii</h1>
+    </div>
+</template>
