3c4ead2aecc19f94064251f565e25fdd3b366961
kazuma01 <kazumagt048@gmail.com>
2026-05-04 17:36:16 -0300

    Api v5.00

--- ApiV1.0/frontend/src/components/Home.vue
+++ ApiV1.0/frontend/src/components/Home.vue
@@ -1,7 +1,41 @@
+
+
 <!-- https://pt.vuejs.org/tutorial/#step-7 -->
 
 <template>
     <div>
-        <h1>oiiii</h1>
+        <form @submit.prevent="Newtext">
+            <input type="text" v-model="Text" required placeholder="Digite aqui">
+            <button>Salva</button>
+        </form>
+        <ul>
+            <li v-for="simples in completo" :key="simples.id">
+                <input type="checkbox" v-model="simples.dote">
+                <span :class="{dote: simples.dote}">{{ simples.text }}</span>
+                <button @click="Removeritem(simples)">Remover</button>
+            </li>
+        </ul>
     </div>
 </template>
+
+<script>
+let id = 0;
+export default{
+    data(){
+        return{
+            Text: "",
+            completo: [
+                {id: id++,text: "primiro texto"}
+            ]
+        }
+    },methods: {
+        Newtext(){
+            this.completo.push({id: id++, text: this.Text}),
+            this.Text = "";
+        },
+        Removeritem(simples){
+            this.completo = this.completo.filter((t) => t != simples)
+        }
+    }
+}
+</script>
