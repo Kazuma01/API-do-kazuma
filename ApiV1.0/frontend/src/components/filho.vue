931b804f1fe66268495600b268cea58cf1429f66
kazuma01 <kazumagt048@gmail.com>
2026-05-07 17:21:03 -0300

    Api V5.02.02 back end depois

---
+++ ApiV1.0/frontend/src/components/filho.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>{{ mgn || "hello world" }}</h1>
+</template>
+
+<script>
+export default{
+    props:{
+        Mgn: String
+    }
+}
+</script>
--- ApiV1.0/frontend/src/router/index.js
+++ ApiV1.0/frontend/src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
   {
     path: "/page",
     component: page
-  }
+  },
 ];
 
 
