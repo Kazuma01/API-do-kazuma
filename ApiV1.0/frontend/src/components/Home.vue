55dba195328b5c6a93c8fb6358cf6b36eccbd2e5
kazuma01 <kazumagt048@gmail.com>
2026-05-11 17:44:37 -0300

    Api V5.3 prepara o back end

--- ApiV1.0/frontend/src/components/Home.vue
+++ ApiV1.0/frontend/src/components/Home.vue
@@ -1,60 +1,32 @@
-
-
-<!-- https://pt.vuejs.org/tutorial/#step-7 -->
-
 <template>
-    <form @submit.prevent="NewItList">
-        <input v-model="Newitem" required placeholder="ADD um novo texto"> <br>
-        <button>Add novo item</button>
-    </form>
-    <ul>
-        <li v-for="item in updatefortext" :key="item.id">
-            <input type="checkbox" v-model="item.done">
-            <span :class="{ done: item.done }">{{ item.text }}</span>
-            <button @click="RemverItemList(item)">X</button>
-        </li>
-    </ul>
-    <button @click="hideAllText = !hideAllText">
-        {{ hideAllText ? "Text Not all" : "Text All" }}
-    </button>
+    <div>
+        <form @submit.prevent="Dadoslist">
+            <input type="text" v-model="Name" required placeholder="Digite seu nome">
+            <input type="text" v-model="Idade" required placeholder="Digite sua idade">
+            <button type="submit">Enviar</button>
+        </form> <br>
+
+    </div>
 </template>
-
 <script>
-
-let id = 0;
-
-
 export default{
     data(){
-        return{ 
-            Newitem: " ",
-            hideAllText: false,
-            itemsList: []
+        return{
+            Name: "",
+            Idade: ""
         }
     },
-    computed:{
-        updatefortext(){
-            return this.hideAllText 
-            ? this.itemsList.filter((t) => !t.done)
-            : this.itemsList
-        }
-    }
-    ,
     methods:{
-        NewItList(){if(this.Newitem !== " "){
-            this.itemsList.push({id: id++,text: this.Newitem, done: false})
-        }    
-        this.Newitem = ""
-    },
-        RemverItemList(item){
-            this.itemsList = this.itemsList.filter((t) => t != item)
+        Dadoslist(){
+            const data = {
+                name: this.Name,
+                idade: this.Idade
+            }
+            this.Name = "";
+            this.Idade = "";
+                console.log(data);
+
         }
     }
 }
 </script>
-
-<style>
-.done{
-    text-decoration: line-through;
-}
-</style>
